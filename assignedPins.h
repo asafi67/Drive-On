680d545c1dd1cb10c0ec864335bee61076256474
vngo00 <vngo12@sfsu.edu>
2023-12-11 15:54:28 -0800

    put pin assignments in separeate file

---
+++ assignedPins.h
@@ -0,0 +1,27 @@
+#ifndef _ASSIGNEDPIN_H_
+#define _ASSIGNEDPIN_H_
+
+
+
+#define HIGH 1
+#define LOW 0
+
+#define LINE_SENSOR_LEFT 18
+#define LINE_SENSOR_MIDDLE 22
+#define LINE_SENSOR_RIGHT 23
+
+
+#define IR_SENSOR_FRONT 17 
+#define IR_SENSOR_LEFT  6 
+#define IR_SENSOR_SIDE  5 
+
+//#define SONAR_TRIGGER 0
+//#define SONAR_ECHO    5
+
+#define IN_BTN 25
+
+
+
+
+
+#endif
